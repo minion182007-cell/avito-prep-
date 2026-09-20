bf79f9f6e4c9f49641a22c3f28b93f3facd24939
Кирилл Кадетов <minion18.2007@gmail.com>
2026-09-20 14:34:33 +0300

    ping: method filtering via route pattern

--- projects/ping/main.go
+++ projects/ping/main.go
@@ -11,12 +11,13 @@ type pingResponse struct {
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(pingResponse{Status: "ok"})
 }
 func main() {
 	log.Println("start")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/ping", pingHandler)
+	mux.HandleFunc("GET /ping", pingHandler)
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
