20989451662339ce38b46f4f970a0675fd00cdd7
Кирилл Кадетов <minion18.2007@gmail.com>
2026-09-20 13:37:29 +0300

    ping: plain text response

--- projects/ping/main.go
+++ projects/ping/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok"))
+}
 func main() {
 	log.Println("start")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", pingHandler)
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
