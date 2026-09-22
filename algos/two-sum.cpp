10b885651b83926b2b54fcbd6647b49c5c3d57e2
minion182007-cell <minion18.2007@gmail.com>
2026-09-22 13:44:47 +0300

    Update two-sum.cpp

--- algos/two-sum.cpp
+++ algos/two-sum.cpp
@@ -10,3 +10,19 @@ public:
     }
     
 };
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> seen;
+      for (int i =0; i<nums.size();i++){
+        int need = target - nums[i];
+            if(seen.count(need) ){
+                return {seen[need],i};
+            }else{
+                seen[nums[i]]=i;
+            }
+        }
+        return {};
+    }
+    
+};
